01b8ab2e69e4a8c376196290f4119ac9799d0a79
Rayyan Anwar <114052246+Rayx404@users.noreply.github.com>
2024-02-20 09:33:20 +0000

    comparision

---
+++ 01-basics/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+//console.log(2 != 1)
+//console.log(2 == 1)
+
+console.log("2" == 1)
+
+console.log("2" > "abc")
+
+console.log("2" === 2)
+
+console.log("2" == 2)
