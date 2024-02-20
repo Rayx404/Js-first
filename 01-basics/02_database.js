0570eadd8d6824827371ffefb0b9223745c025fb
Rayyan Anwar <114052246+Rayx404@users.noreply.github.com>
2024-02-20 07:36:37 +0000

    database

---
+++ 01-basics/02_database.js
@@ -0,0 +1,13 @@
+
+console.log(typeof "rayyan")
+
+console.log(typeof 18)
+
+console.log(typeof true)
+
+console.log(typeof null)  // its datatype is object, therefore it is the language error in js
+
+console.log(typeof undefined)
+
+console.log(typeof rayx)
+
