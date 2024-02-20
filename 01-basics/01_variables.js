fe7e81795dfd82ea02ed44b19015d603d569d856
Rayyan Anwar <114052246+Rayx404@users.noreply.github.com>
2024-02-20 07:35:26 +0000

    hello

--- 01-basics/01_variables.js
+++ 01-basics/01_variables.js
@@ -1,1 +1,16 @@
+const accountId = 14453
+let accountEmail = "rayyan25@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ranchi"
+let accountState;
+
+// accountId = 2 not Allowed
+
+accountEmail = "Aaya@gmail.com"
+accountPassword = "w222"
+accountCity ="XYZ"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
 
