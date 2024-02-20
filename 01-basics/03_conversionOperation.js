5ab22fb36746e09c5efab76c7dedc50834f50bec
Rayyan Anwar <114052246+Rayx404@users.noreply.github.com>
2024-02-20 08:44:05 +0000

    conversionOperation

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(score)
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// ** agar null value dal de to 0 aaiga
+//** agar undefined dal de to NaN aai ga
+// ** 33abc ka bhi NaN hai
+// ** agar boolean vale hoti to true ka 1 and false ka 0 aata
+// ** agar string hoti to vo number mai convert nahi ho sakti to NaN aaiga
+
