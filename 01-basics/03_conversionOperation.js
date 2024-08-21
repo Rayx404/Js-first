35e0eb07ad15d3ab74c7ad311a3c6484b1d26304
Rayyan Anwar <114052246+Rayx404@users.noreply.github.com>
2024-08-21 08:33:37 +0000

    added few examples of conversions

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -15,3 +15,77 @@ console.log(valueInNumber)
 // ** agar boolean vale hoti to true ka 1 and false ka 0 aata
 // ** agar string hoti to vo number mai convert nahi ho sakti to NaN aaiga
 
+
+
+let draws = 33
+console.log(typeof draws)    // number
+console.log(typeof(draws))   // number
+
+
+let age = "55"
+console.log(typeof age);    // string
+console.log(typeof(age));   // string
+
+
+
+let point = "abc"
+let valueInNumbers = Number(point)     // ******* by using this we can change string to number  ********
+console.log(typeof valueInNumbers);    // number
+
+
+// more example
+
+let name = "rayx"
+let naam = Number(name)
+console.log(typeof naam)    // Number
+
+console.log(naam)    // NaN (Not A Number)  datatypes changes but value remain same 
+
+/*
+
+** when we convert string to number,  its datatypes changes to number but it value will be printed as Not a Number **** 
+
+*/
+
+/*
+        conversion:- to number
+
+        output value:-
+
+        "33" => 33
+        "33abc" => NaN
+        true => 1
+        false => 0
+        null => 0
+        undefined => NaN
+
+
+        conversion:- to Boolean
+
+        output value :-
+
+        1 => true
+        0 => false 
+        " " => false
+        "rayx" => true
+
+*/
+
+
+let value = 55
+let stringValue = String(value)     // to change to string
+console.log(stringValue)            // 55
+console.log(typeof stringValue)     // string
+
+
+let kill = 0
+let booleanKill = Boolean (kill)        // to change to boolean
+console.log(booleanKill)                // false
+console.log(typeof booleanKill);        // boolean
+
+
+let gf = "xyz"
+let booleanGf = Boolean(gf)
+console.log(booleanGf);         // true 
+console.log(typeof booleanGf);  // boolean
+
