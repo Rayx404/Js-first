f1ccf2a991ba35855992e84f4112f4271c47fbb0
Rayyan Anwar <114052246+Rayx404@users.noreply.github.com>
2024-08-21 07:56:40 +0000

    datatypes add from vs

--- 01-basics/02_database.js
+++ 01-basics/02_database.js
@@ -11,3 +11,31 @@ console.log(typeof undefined)
 
 console.log(typeof rayx)
 
+
+// code readibility should be high
+
+let name = "rayx"  // string
+let age = 18    // number
+let isMale = true    // boolean
+
+/* 
+number ==> 2 to power 53 
+bigint => large number used in stocks
+string => "xyz"
+boolean => true or false
+null => standalone value, representation of an empty value eg:- let age = null
+undefined =>   value not given eg:- let name = " "
+symbol => uniqueness 
+*/
+
+
+console.log(typeof name);
+
+console.log(typeof age);
+
+console.log(typeof isMale)
+
+console.log(typeof null);  //error in js language error becaause it gives object
+
+console.log(typeof undefined);  // undefined
+
