f19e7c5df4a1a96e2c040cbd7768641a0ee9587f
Rayyan Anwar <114052246+Rayx404@users.noreply.github.com>
2024-08-22 08:04:56 +0000

    more example and summed up lesson

--- 01-basics/04_comparision.js
+++ 01-basics/04_comparision.js
@@ -10,3 +10,38 @@ console.log("2" > "abc")
 console.log("2" === 2)
 
 console.log("2" == 2)
+
+// more example and summed up lesson 
+
+console.log(2 > 1)      // true
+console.log(2 >= 1)     // true
+console.log(2 == 1);    // false
+console.log(2 != 1);    // true
+
+console.log("2" > 1);   // true
+console.log("02" < 1)   // false
+
+// to compare two values the datatypes should be same 
+
+
+console.log(null > 0);      // false
+console.log(null == 0)      // false
+console.log(null >= 0);     // true
+console.log(null < 0);      // false
+console.log(null <= 0)      // true
+
+// the reason is that an equality check (==) and the comparision check (> < >= <=) works differently.
+// comparision converts null to a number, trerating it as 0.
+// thats why on (3 & 5) null >= 0 & null <= 0 is true and on (1 & 4) null > 0 & null < 0 is false
+
+// same things  happens under undefined 
+
+
+// strict check ===
+
+console.log("2" == 2);  // true
+
+console.log("2" === 2);  // false
+
+// === checks values as well as datatypes 
+
